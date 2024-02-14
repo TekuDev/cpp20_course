11b904076977c52c2991a259f194fd291623f61f
Teku <teku.developer@gmail.com>
2024-02-14 09:46:31 +0000

    concepts example

--- concepts/main.cpp
+++ concepts/main.cpp
@@ -1,5 +1,21 @@
+#include <concepts>
+#include <iostream>
 
+template <typename T>
+concept Numeric = std::integral<T> || std::floating_point<T>;
+
+template <Numeric T>
+T Average(T x, T y) {
+  return (x + y) / 2;
+}
 
 int main() {
-    
+    //std::cout << "Average(2, 17): " << Average(2, 17) << std::end;
+    //std::cout << "Average(4.2f, 8.1f): " << Average(4.2f, 8.1f) << std::end;
+    //std::cout << "Average(12.2, 22.4): " << Average(12.2, 22.4) << std::end;
+
+    Average(2, 17);
+    Average(4.2f, 8.1f);
+    Average(12.2, 22.4);
+    Average("hello", "world");
 }
